11cb89ff1cae768b02f2040f20ade31bea2bc20e
Harshita0109 <Harshita.1128@gmail.com>
2020-07-17 18:46:20 +0530

    Final Project

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,25 +1,26 @@
 #!/usr/bin/env bash
 
-echo "[Welcome to Guessing game v0.1]"
-
-function ask {
-	echo "Please enter the number of files in the current directory:"
-	read guess
+echo "|-----------------Welcome to the Guessing Game------------------|"
+echo "|You have to guess the number of files in your current directory|"
+function game {
+	read -p "Please enter the number of files: " guess
     files=$(ls -1 | wc -l)
 }
-
-ask
-
+game
 while [[ $guess -ne $files ]]
 do
 	if [[ $guess -lt $files ]] 
 	then
-		echo "Too low."
+	    echo "Wrong Guess"
+		echo "Try with a Higher value."
 	else
-		echo "Too high."
+	    echo "Wrong Guess"
+		echo "Try with a Lower value."
 	fi
-	ask
+	game
 done
-
-echo "Well done! It is the correct answer, here is the list of files:"
-echo "---" && ls -1
+echo " "
+echo "***Congratulations***"
+echo "The value you entered is correct"
+echo "The list of files are: " 
+ls -1
